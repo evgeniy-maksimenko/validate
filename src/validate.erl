1fc873aa92bb81b305df8f4d7eff1d9b9675b033
user <evgenij.maksimenko.01@privatbank.ua>
2014-11-11 15:54:00 +0200

    bag errors

--- src/validate.erl
+++ src/validate.erl
@@ -13,13 +13,39 @@
 -module(validate).
 -export([main/2, start/2, check_rule/4, check_source/2]).
 
--spec main(Source::binary() | list() | integer() | float(), Rules::list()) ->
-{error, Type::atom()} | ok .
+-type source()    :: proplists().
+-type proplists() :: [kvtuple()].
+-type kvtuple()   :: {key(), value()}.
+-type key()       :: binary().
+-type value()     :: binary().
+
+-type rules()     :: ruleslist().
+-type ruleslist() :: [pvtuple()].
+-type pvtuple()   :: {property(), amount()}.
+-type property()  :: length | length_range | bit_size | bit_size_range | value_range | regexp.
+-type amount()    :: binary() | list() | integer() | float().
+
+-type error()           :: errortuple_kv() | ok.
+-type errortuple_kv()   :: {error, property() | kv_error_tuple()}.
+-type kv_error_tuple()  :: {key_error(), value_error()}.
+-type key_error()       :: wrong_format.
+-type value_error()     :: datatype.
+
+-type error_kv()        :: errortuple() | ok.
+-type errortuple()      :: {error, property()}.
+
+-type data()                :: list() | integer() | tuple().
+
+-type ck_source_property()  :: property().
+-type ck_source_tuple()     :: {ck_source_key(), ck_source_value()} |  nomatch | boolean().
+-type ck_source_key()       :: match | integer().
+-type ck_source_value()     :: property() | integer() | tuple().
+
+-spec main(Source::source(), Rules::rules()) -> error().
 main(Source,Rules) ->
   start(Source, Rules).
 
--spec start(Source::binary() | list() | integer() | float(), Rules::list()) ->
-  {error, Type::atom()} | ok .
+-spec start(Source::source(), Rules::rules()) -> error().
 start(Source, Rules) ->
   try
     List = [ check_rule(Type, Type, Data, Source) || {Type, Data}<-Rules ],
@@ -32,13 +58,12 @@ start(Source, Rules) ->
     _ : _Reason -> {error, {wrong_format,datatype}}
   end.
 
--spec isset_error(boolean(), atom(), boolean(), list()) -> atom() | list().
+-spec isset_error(boolean(),Listskeyfind::tuple(),boolean(),Match::tuple()) -> tuple() | ok.
 isset_error(true, Listskeyfind, _, _) -> Listskeyfind;
-isset_error(false, _Listskeyfind, true, Match) -> Match;
-isset_error(false, _Listskeyfind, false, _Match) -> ok.
+isset_error(false, _, true, Match) -> Match;
+isset_error(false, _, false, _) -> ok.
 
--spec check_rule(atom(), Type::atom(), Data::binary() | list() | integer() | float(), Source::binary() | list() | integer() | float()) ->
-  {error, Type::atom()} | ok .
+-spec check_rule(property(), Type::property(), Data::data(), Source::amount()) -> error_kv().
 check_rule(length, Type, Data, Source)
   when (is_integer(Data) and is_list(Source)) ->
   check_source(length(Source) == Data, Type);
@@ -65,11 +90,11 @@ check_rule(regexp, Type, Data, Source)
   check_source(binary:match(Source, Data), Type);
 check_rule(Type, _Type, _Data, _Source) -> {error, Type}.
 
--spec check_source({atom(), atom()}, atom()) -> {error, Type::atom()} | ok .
-check_source(nomatch, _Type) -> {error, regexp};
-check_source({match, _Regexp}, _Type) -> ok;
-check_source(true, _Type) -> ok;
-check_source(false, Type) -> {error, Type};
-check_source({_Key,_Val}, _Type) -> ok.
+-spec check_source(ck_source_tuple(),ck_source_property()) -> error_kv().
+check_source(nomatch, _Type)            -> {error, regexp};
+check_source({match,  _Regexp}, _Type)  -> ok;
+check_source(true,    _Type)            -> ok;
+check_source(false,   Type)             -> {error, Type};
+check_source({_Key,   _Val}, _Type)     -> ok.
 
 
--- src/validate_wrapper.erl
+++ src/validate_wrapper.erl
@@ -18,9 +18,26 @@
 %% ]
 
 -module(validate_wrapper).
-
 -export([start/2]).
--spec start(Source::binary() | list() | integer() | float(), Rules::list()) ->
-  {error, Type::atom()} | ok .
+
+-type source()    :: proplists().
+-type proplists() :: [kvtuple()].
+-type kvtuple()   :: {key(), value()}.
+-type key()       :: binary().
+-type value()     :: binary().
+
+-type rules()     :: ruleslist().
+-type ruleslist() :: [pvtuple()].
+-type pvtuple()   :: {property(), amount()}.
+-type property()  :: length | length_range | bit_size | bit_size_range | value_range | regexp.
+-type amount()    :: binary() | list() | integer() | float().
+
+-type error()           :: errortuple_kv() | ok.
+-type errortuple_kv()   :: {error, property() | kv_error_tuple()}.
+-type kv_error_tuple()  :: {key_error(), value_error()}.
+-type key_error()       :: wrong_format.
+-type value_error()     :: datatype.
+
+-spec start(Source::source(), Rules::rules()) -> error().
 start(Source, Rules) ->
   validate:main(Source, Rules).
